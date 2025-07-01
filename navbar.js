a1499b98eb44264a97f93b358603589f331b3957
sarada-sivasailam <163179852+sarada-sivasailam@users.noreply.github.com>
2025-07-01 12:58:31 -0500

    Delete navbar.js

--- navbar.js
+++
@@ -1,8 +0,0 @@
-fetch('/navbar.html')
-  .then(response => response.text())
-  .then(data => {
-    const placeholder = document.getElementById('navbar-placeholder');
-    if (placeholder) {
-      placeholder.innerHTML = data;
-    }
-  });
