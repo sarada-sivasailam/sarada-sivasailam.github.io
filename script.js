9c385bc4b09105dde01b7d6918e5e31f64985563
sarada-sivasailam <163179852+sarada-sivasailam@users.noreply.github.com>
2025-07-01 14:03:44 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,41 @@
-function openProject(url) {
-  window.open(url, '_blank');
+function openWindow(id) {
+  document.getElementById(id).style.display = 'flex';
 }
 
-function openPage(page) {
-  window.location.href = page;
+function closeWindow(id) {
+  document.getElementById(id).style.display = 'none';
 }
 
-function openLink(link) {
-  window.open(link, '_blank');
+function openSpotify() {
+  window.open('https://open.spotify.com/playlist/1YZUuEKazJpGqbxfM36Gl2?si=EBfXRqDbS92zfHrBJWpuPg', '_blank');
 }
+
+function toggleMode() {
+  document.body.classList.toggle('dark-mode');
+}
+
+// Make windows draggable
+const windows = document.querySelectorAll('.window');
+windows.forEach(win => {
+  const header = win.querySelector('.window-header');
+  header.onmousedown = function (e) {
+    const offsetX = e.clientX - win.offsetLeft;
+    const offsetY = e.clientY - win.offsetTop;
+
+    function moveAt(e) {
+      win.style.left = e.clientX - offsetX + 'px';
+      win.style.top = e.clientY - offsetY + 'px';
+    }
+
+    function onMouseMove(e) {
+      moveAt(e);
+    }
+
+    document.addEventListener('mousemove', onMouseMove);
+
+    document.onmouseup = function () {
+      document.removeEventListener('mousemove', onMouseMove);
+      document.onmouseup = null;
+    };
+  };
+});
