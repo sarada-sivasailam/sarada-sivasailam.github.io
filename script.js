baf6d820064fa041646aecb8f53150233319a206
sarada-sivasailam <163179852+sarada-sivasailam@users.noreply.github.com>
2025-07-01 13:13:23 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+function openProject(url) {
+  window.open(url, '_blank');
+}
+
+function openPage(page) {
+  window.location.href = page;
+}
+
+function openLink(link) {
+  window.open(link, '_blank');
+}
